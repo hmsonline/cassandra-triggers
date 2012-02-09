1297aa1116e31e9f4e5ae5298ce34924c43a63cb
ducquoc <ducquoc.vn@gmail.com>
2012-02-10 02:56:36 +0700

    [REMED-133] added AOP handle for general exception

--- src/test/java/com/hmsonline/cassandra/triggers/TriggerTest.java
+++ src/test/java/com/hmsonline/cassandra/triggers/TriggerTest.java
@@ -2,4 +2,9 @@ package com.hmsonline.cassandra.triggers;
 
 public class TriggerTest {
 
+    @org.junit.Test
+    public void testThrowingExceptionWhenInsert() {
+        org.junit.Assert.assertTrue(true);
+    }
+
 }
