86afa098ec5d44f069aa70021979a66a1cbb86e9
Isaac <irieksts@hmsonline.com>
2012-02-10 08:58:45 -0500

    Removing label

--- src/main/java/com/hmsonline/cassandra/triggers/TriggerStore.java
+++ src/main/java/com/hmsonline/cassandra/triggers/TriggerStore.java
@@ -19,70 +19,78 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TriggerStore extends CassandraStore {
-    private static Logger logger = LoggerFactory.getLogger(TriggerStore.class);
+  private static Logger logger = LoggerFactory.getLogger(TriggerStore.class);
 
-    public static final String KEYSPACE = "triggers";
-    public static final String COLUMN_FAMILY = "Triggers";
-    public static final String ENABLED = "enabled";
-    public static final String PAUSED = "PAUSED";
-    private static TriggerStore instance = null;
+  public static final String KEYSPACE = "triggers";
+  public static final String COLUMN_FAMILY = "Triggers";
+  public static final String ENABLED = "enabled";
+  public static final String PAUSED = "PAUSED";
+  private static TriggerStore instance = null;
 
-    public TriggerStore(String keyspace, String columnFamily) throws Exception{
-        super(keyspace, columnFamily);
-        logger.debug("Instantiated trigger store.");
+  public TriggerStore(String keyspace, String columnFamily) throws Exception {
+    super(keyspace, columnFamily);
+    logger.debug("Instantiated trigger store.");
+  }
+
+  public static synchronized TriggerStore getStore() throws Exception {
+    if (instance == null)
+      instance = new TriggerStore(KEYSPACE, COLUMN_FAMILY);
+    return instance;
+  }
+
+  @SuppressWarnings("unchecked")
+  public static Trigger getTrigger(String triggerClass) throws Exception {
+    Class<Trigger> clazz = (Class<Trigger>) Class.forName(triggerClass);
+    return clazz.newInstance();
+  }
+
+  public Map<String, List<Trigger>> getTriggers() throws Exception {
+    // TODO: Cache this.
+    Map<String, List<Trigger>> triggerMap = new HashMap<String, List<Trigger>>();
+    SlicePredicate predicate = new SlicePredicate();
+    SliceRange range = new SliceRange(ByteBufferUtil.bytes(""), ByteBufferUtil.bytes(""), false, 10);
+    predicate.setSlice_range(range);
+
+    KeyRange keyRange = new KeyRange(1000);
+    keyRange.setStart_key(ByteBufferUtil.bytes(""));
+    keyRange.setEnd_key(ByteBufferUtil.EMPTY_BYTE_BUFFER);
+    ColumnParent parent = new ColumnParent(COLUMN_FAMILY);
+    List<KeySlice> rows = getConnection(KEYSPACE).get_range_slices(parent, predicate, keyRange,
+                                                                   ConsistencyLevel.ALL);
+    for (KeySlice slice : rows) {
+      String columnFamily = ByteBufferUtil.string(slice.key);
+      triggerMap.put(columnFamily, processRow(slice));
     }
-    
-    public static synchronized TriggerStore getStore() throws Exception {
-        if (instance == null)
-            instance = new TriggerStore(KEYSPACE, COLUMN_FAMILY);
-        return instance;
-    }
+    return triggerMap;
+  }
 
-    @SuppressWarnings("unchecked")
-    public static Trigger getTrigger(String triggerClass) throws Exception {
-        Class<Trigger> clazz = (Class<Trigger>) Class.forName(triggerClass);
-        return clazz.newInstance();
-    }
+  @SuppressWarnings({
+      "unchecked", "rawtypes"
+  })
+  private List<Trigger> processRow(KeySlice slice) throws Exception {
 
-    @SuppressWarnings({
-        "unchecked", "rawtypes"
-    })
-    public Map<String, List<Trigger>> getTriggers() throws Exception {
-        // TODO: Cache this.
-        Map<String, List<Trigger>> triggerMap = new HashMap<String, List<Trigger>>();
-        SlicePredicate predicate = new SlicePredicate();
-        SliceRange range = new SliceRange(ByteBufferUtil.bytes(""), ByteBufferUtil.bytes(""), false, 10);
-        predicate.setSlice_range(range);
-
-        KeyRange keyRange = new KeyRange(1000);
-        keyRange.setStart_key(ByteBufferUtil.bytes(""));
-        keyRange.setEnd_key(ByteBufferUtil.EMPTY_BYTE_BUFFER);
-        ColumnParent parent = new ColumnParent(COLUMN_FAMILY);
-        List<KeySlice> rows = getConnection(KEYSPACE).get_range_slices(parent, predicate, keyRange,
-                ConsistencyLevel.ALL);
-        rowsLoop:
-        for (KeySlice slice : rows) {
-            String columnFamily = ByteBufferUtil.string(slice.key);
-            List<Trigger> triggers = new ArrayList<Trigger>();
-            for (ColumnOrSuperColumn column : slice.columns) {
-                String className = ByteBufferUtil.string(column.column.name);
-                String enabled = ByteBufferUtil.string(column.column.value);
-                if(PAUSED.equals(StringUtils.upperCase(className))){
-                  if(ENABLED.equals(enabled)) {
-                    triggerMap.put(columnFamily, new ArrayList(Arrays.asList(new Trigger [] {new PausedTrigger()})));
-                    continue rowsLoop;
-                  } else {
-                    continue;
-                  }
-                } 
-                else {
-                  if (enabled.equals(ENABLED)) {
-                    triggers.add(getTrigger(className));
-                  }
-                }
-            }
-            triggerMap.put(columnFamily, triggers);
+    List<Trigger> triggers = new ArrayList<Trigger>();
+    for (ColumnOrSuperColumn column : slice.columns) {
+      String className = ByteBufferUtil.string(column.column.name);
+      String enabled = ByteBufferUtil.string(column.column.value);
+      if (PAUSED.equals(StringUtils.upperCase(className))) {
+        if (ENABLED.equals(enabled)) {
+          return new ArrayList(Arrays.asList(new Trigger[] {
+              new PausedTrigger()
+          }));
         }
-        return triggerMap;
+        else {
+          // The pause is disabled so we do not want to process this as a normal
+          // class.  We should go to the next column.
+          continue;
+        }
+      }
+      else {
+        if (enabled.equals(ENABLED)) {
+          triggers.add(getTrigger(className));
+        }
+      }
     }
+    return triggers;
+  }
 }
